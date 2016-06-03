bc45b44f4fb102f8e236abaa466e9738c2a7ff5a
Andrew Russo <russspruce@gmail.com>
2016-06-03 12:51:36 -0700

    Successfully separated all files into idividuals and successfully have infomation display on main page.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,43 +1,7 @@
 import { Component, EventEmitter } from 'angular2/core';
-// import { MealListComponent } from './meal-list.component';
-// import { Meal } from './meal.model';
+import { MealListComponent } from './meal-list.component';
+import { Meal } from './meal.model';
 
-@Component({
-  selector: 'meal-display',
-  inputs: ['meal'],
-  template: `
-    <h3> {{ meal.name }} </h3>
-    `
-})
-
-export class MealComponent {
-  public meal: Meal;
-}
-
-@Component({
-  selector: 'meal-list',
-  inputs: ['mealList'],
-  directives: [MealComponent],
-  template: `
-    <meal-display *ngFor="#currentMeal of mealList" (click)="mealClicked(currentMeal)" [class.selected]="currentMeal === selectedMeal"
-    [meal]="currentMeal">
-      </meal-display>
-  `
-})
-
-export class MealListComponent {
-  public mealList: Meal[];
-  public onMealSelect: EventEmitter<Meal>;
-  public selectedMeal: Meal;
-  constructor() {
-    this.onMealSelect = new EventEmitter();
-  }
-  mealClicked(clickedMeal: Meal): void {
-    console.log('child', clickedMeal);
-    this.selectedMeal = clickedMeal;
-    this.onMealSelect.emit(clickedMeal);
-  }
-}
 
 @Component({
   selector: 'my-app',
@@ -63,8 +27,4 @@ export class AppComponent {
   mealWasSelected(clickedMeal: Meal): void {
     console.log('parent', clickedMeal);
   }
-}
-
-export class Meal {
-  constructor(public name: string, public description: string, public date: string, public calories: number) {}
 }
---
+++ app/meal.model.ts
@@ -0,0 +1,3 @@
+export class Meal {
+  constructor(public name: string, public description: string, public date: string, public calories: number) {}
+}
