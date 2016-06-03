cdb574988b69b130abeb78ea207a529abe471d6d
Andrew Russo <russspruce@gmail.com>
2016-06-03 11:29:13 -0700

    Started working from the start and have app properly displaying a message.

---
+++ app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter } from 'angular2/core';
+// import { MealListComponent } from './meal-list.component';
+// import { Meal } from './meal.model';
+
+
+@Component({
+  selector: 'my-app',
+  // directives: [MealListComponent],
+  template: `
+  <div class="container">
+    <h1>Food Tracker</h1>
+    <meal-list [mealList] = "meals">
+    </meal-list>
+  </div>
+  `
+})
+
+export class AppComponent {
+  // public meals: Meal[];
+  // constructor() {
+  //   this.meals = [
+  //     new Meal("Egg and Cheese Sandwich", "A tasty breakfast treat", "Monday", 340),
+  //     new Meal("Cantaloupe", "A sweet fruit and a good snack", "Tuesday", 23),
+  //     new Meal("Almond Snack Bag", "For when you want somethin in between meals", "Wednesday", 100)
+  //   ];
+  // }
+}
