b98e3825e325119c18b82100af3c990e38a40c5e
Andrew Russo <russspruce@gmail.com>
2016-06-03 11:39:41 -0700

    Have app displaying values of meals on the page.

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,19 +9,24 @@ import { Component, EventEmitter } from 'angular2/core';
   template: `
   <div class="container">
     <h1>Food Tracker</h1>
-    <meal-list [mealList] = "meals">
-    </meal-list>
+    <div *ngFor="#meal of meals">
+  <h3>{{ meal.name }}</h3>
+</div>
   </div>
   `
 })
 
 export class AppComponent {
-  // public meals: Meal[];
-  // constructor() {
-  //   this.meals = [
-  //     new Meal("Egg and Cheese Sandwich", "A tasty breakfast treat", "Monday", 340),
-  //     new Meal("Cantaloupe", "A sweet fruit and a good snack", "Tuesday", 23),
-  //     new Meal("Almond Snack Bag", "For when you want somethin in between meals", "Wednesday", 100)
-  //   ];
-  // }
+  public meals: Meal[];
+  constructor() {
+    this.meals = [
+      new Meal("Egg and Cheese Sandwich", "A tasty breakfast treat", "Monday", 340),
+      new Meal("Cantaloupe", "A sweet fruit and a good snack", "Tuesday", 23),
+      new Meal("Almond Snack Bag", "For when you want somethin in between meals", "Wednesday", 100)
+    ];
+  }
+}
+
+export class Meal {
+  constructor(public name: string, public description: string, public date: string, public calories: number) {}
 }
